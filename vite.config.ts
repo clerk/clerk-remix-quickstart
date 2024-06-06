cd0ff3352f991d7df0431faabf937defcb356270
Stefanos Anagnostou <stefanos@clerk.dev>
2024-06-06 15:46:23 +0300

    Update README

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     remix({
         ignoredRouteFiles: ["**/.*"],
